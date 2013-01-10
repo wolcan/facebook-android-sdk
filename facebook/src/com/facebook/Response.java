33043ccb2f66a10b18b508b405949e20d009421f
Watson <watson@appsynth.net>
2013-01-10 16:34:19 +0700

    removed lints
    Signed-off-by: Watson <watson@appsynth.net>

--- facebook/src/com/facebook/Response.java
+++ facebook/src/com/facebook/Response.java
@@ -260,7 +260,6 @@ public class Response {
         return responseCache;
     }
 
-    @SuppressWarnings("resource")
     static List<Response> fromHttpConnection(HttpURLConnection connection, RequestBatch requests) {
         InputStream stream = null;
 
